43239a12053cc997a7f787d634da7dd46d915ac3
daryavolnaya <129992395+daryavolnaya@users.noreply.github.com>
2023-06-17 13:43:25 +0200

    Added link to Github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">  
         <SearchEngine />
+       <div className='Github'> <a href="https://github.com/daryavolnaya/react-weather" target="_blank" rel="noreferrer">Open-source code</a>, by <a href="https://github.com/daryavolnaya" target="_blank" rel="noreferrer">Daria Romaniuk</a></div>
       </header>
     </div>
   );
